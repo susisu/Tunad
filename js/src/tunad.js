ec769d9bb20a96601a617f594c9ce478f007bf83
Susisu <susisu2413@yahoo.co.jp>
2013-09-01 19:00:04 +0900

    add if and while

--- js/src/tunad.js
+++ js/src/tunad.js
@@ -3,194 +3,262 @@
 	Copyright(C) 2013 Susisu
 */
 
+var Tunad = new Object();
+
+Tunad.Action = function(func)
+{
+	this.func = func;
+	this.onComplete = undefined;
+}
+Tunad.Action.prototype.play = function(env, arg)
+{
+	this.func.call(env, arg);
+}
+Tunad.Action.prototype.complete = function(value)
+{
+	if(this.onComplete)
+	{
+		this.onComplete(value);
+		this.onComplete = null;
+	}
+}
+Tunad.Action.prototype.apply = function(arg)
+{
+	var action = this;
+	var wrapper = new Action(function(_)
+	{
+		action.onComplete = function(value)
+		{
+			wrapper.complete(value);
+		};
+		action.play(this, arg);
+	});
+	return wrapper;
+};
+
+Tunad._void = function()
+{
+	var action = new Action(function(_)
+	{
+		action.complete();
+	});
+	return action;
+};
+
+Tunad._return = function()
+{
+	var action = new Action(function(arg)
+	{
+		action.complete(arg);
+	});
+	return action;
+};
+
+Tunad.bind = function(a, b)
+{
+	var action = new Action(function(arg)
+	{
+		var env = this;
+		a.onComplete = function(avalue)
+		{
+			b.onComplete = function(bvalue)
+			{
+				action.complete(bvalue);
+			};
+			b.play(env, avalue);
+		};
+		a.play(env, arg);
+	});
+	return action;
+};
+
+Tunad.mbind = function()
+{
+	if(arguments.length > 0)
+	{
+		var t = arguments[0];
+		for(var i = 1; i < arguments.length; i++)
+		{
+			t = bind(t, arguments[i])
+		}
+		return t;
+	}
+	else
+	{
+		return _void();
+	}
+};
+
+Tunad.async = function()
+{
+	if(arguments.length > 0)
+	{
+		var actions = arguments;
+		var action = new Action(function(arg)
+		{
+			var n = 0;
+			function handler(value)
+			{
+				n++;
+				if(n == actions.length)
+				{
+					action.complete();
+				}
+			}
+			for(var i = 0; i < actions.length; i++)
+			{
+				actions[i].onComplete = handler;
+				actions[i].play(this, arg);
+			}
+		});
+		return action;
+	}
+	else
+	{
+		return _void();
+	}
+};
+
+Tunad.assign = function(name)
+{
+	var action = new Action(function(arg)
+	{
+		this[name] = arg;
+		
+		action.complete();
+	});
+	return action;
+};
+
+Tunad._set = function(name)
+{
+	return function(value)
+	{
+		return bind(value, assign(name));
+	};
+};
+
+Tunad._get = function(name)
+{
+	var action = new Action(function(_)
+	{
+		action.complete(this[name]);
+	});
+	return action;
+};
+
+Tunad.lambda = function(func)
+{
+	var action = new Action(function(arg)
+	{
+		var sub = func(arg);
+		sub.onComplete = function(value)
+		{
+			action.complete(value);
+		};
+		sub.play(this);
+	});
+	return action;
+};
+
+Tunad._do = function(func)
+{
+	var action = new Action(function(arg)
+	{
+		action.complete(func.call(this, arg));
+	});
+	return action;
+};
+
+Tunad._if = function(condition, a, b)
+{
+	var action = new Action(function(arg)
+	{
+		var env = this;
+		condition.onComplete = function(cond)
+		{
+			if(cond)
+			{
+				a.onComplete = function(value)
+				{
+					action.complete(value);
+				}
+				a.play(env);
+			}
+			else
+			{
+				b.onComplete = function(value)
+				{
+					action.complete(value);
+				}
+				b.play(env);
+			}
+		};
+		condition.play(env);
+	});
+	return action;
+};
+
+Tunad._while = function(condition, statement)
+{
+	var action = new Action(function(_)
+	{
+		var env = this;
+		var v;
+		function evalCondition()
+		{
+			condition.onComplete = function(cond)
+			{
+				if(cond)
+				{
+					evalStatement();
+				}
+				else
+				{
+					action.complete(v);
+				}
+			};
+			condition.play(env);
+		}
+		function evalStatement()
+		{
+			statement.onComplete = function(value)
+			{
+				v = value;
+				evalCondition();
+			}
+			statement.play(env);
+		}
+		evalCondition();
+	});
+	return action;
+};
+
+Tunad.print = function()
+{
+	var action = new Action(function(arg)
+	{
+		console.log(String(arg));
+		
+		action.complete();
+	});
+	return action;
+};
+
 function useTunad()
 {
-	function Action(func)
-	{
-		this.func = func;
-		this.onComplete = undefined;
-	}
-	Action.prototype.play = function(env, arg)
-	{
-		this.func.call(env, arg);
-	}
-	Action.prototype.complete = function(value)
-	{
-		if(this.onComplete)
-		{
-			this.onComplete(value);
-			this.onComplete = null;
-		}
-	}
-	Action.prototype.apply = function(arg)
-	{
-		var action = this;
-		var wrapper = new Action(function(_)
-		{
-			action.onComplete = function(value)
-			{
-				wrapper.complete(value);
-			};
-			action.play(this, arg);
-		});
-		return wrapper;
-	}
-	
-	function _void()
-	{
-		var action = new Action(function(_)
-		{
-			action.complete();
-		});
-		return action;
-	}
-	
-	function _return()
-	{
-		var action = new Action(function(arg)
-		{
-			action.complete(arg);
-		});
-		return action;
-	}
-	
-	function bind(a, b)
-	{
-		var action = new Action(function(arg)
-		{
-			var env = this;
-			a.onComplete = function(avalue)
-			{
-				b.onComplete = function(bvalue)
-				{
-					action.complete(bvalue);
-				};
-				b.play(env, avalue);
-			};
-			a.play(env, arg);
-		});
-		return action;
-	}
-	
-	function mbind()
-	{
-		if(arguments.length > 0)
-		{
-			var t = arguments[0];
-			for(var i = 1; i < arguments.length; i++)
-			{
-				t = bind(t, arguments[i])
-			}
-			return t;
-		}
-		else
-		{
-			return _void();
-		}
-	}
-	
-	function async()
-	{
-		if(arguments.length > 0)
-		{
-			var actions = arguments;
-			var action = new Action(function(arg)
-			{
-				var n = 0;
-				function handler(value)
-				{
-					n++;
-					if(n == actions.length)
-					{
-						action.complete();
-					}
-				}
-				for(var i = 0; i < actions.length; i++)
-				{
-					actions[i].onComplete = handler;
-					actions[i].play(this, arg);
-				}
-			});
-			return action;
-		}
-		else
-		{
-			return _void();
-		}
-	}
-	
-	function assign(name)
-	{
-		var action = new Action(function(arg)
-		{
-			this[name] = arg;
-			
-			action.complete();
-		});
-		return action;
-	}
-	
-	function _set(name)
-	{
-		return function(value)
-		{
-			return bind(value, assign(name));
-		};
-	}
-	
-	function _get(name)
-	{
-		var action = new Action(function(_)
-		{
-			action.complete(this[name]);
-		});
-		return action;
-	}
-	
-	function lambda(func)
-	{
-		var action = new Action(function(arg)
-		{
-			var sub = func(arg);
-			sub.onComplete = function(value)
-			{
-				action.complete(value);
-			};
-			sub.play(this);
-		});
-		return action;
-	}
-	
-	function _do(func)
-	{
-		var action = new Action(function(arg)
-		{
-			action.complete(func.call(this, arg));
-		});
-		return action;
-	}
-	
-	function print()
-	{
-		var action = new Action(function(arg)
-		{
-			console.log(String(arg));
-			
-			action.complete();
-		});
-		return action;
-	}
-	
-	this.Action = Action;
-	this._void = _void;
-	this._return = _return;
-	this.bind = bind;
-	this.mbind = mbind;
-	this.async = async;
-	this.assign = assign;
-	this._set = _set;
-	this._get = _get;
-	this.lambda = lambda;
-	this._do = _do;
-	this.print = print;
+	this.Action = Tunad.Action;
+	this._void = Tunad._void;
+	this._return = Tunad._return;
+	this.bind = Tunad.bind;
+	this.mbind = Tunad.mbind;
+	this.async = Tunad.async;
+	this.assign = Tunad.assign;
+	this._set = Tunad._set;
+	this._get = Tunad._get;
+	this.lambda = Tunad.lambda;
+	this._do = Tunad._do;
+	this._if = Tunad._if;
+	this._while = Tunad._while;
+	this.print = Tunad.print;
 }
